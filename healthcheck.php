226a479622124645d17c75b47605ad6561a4221d
Alex Trofimov <alex.trofimov@boonex.com>
2025-08-05 09:56:38 +0300

    Healthcheck #5128

---
+++ healthcheck.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) UNA, Inc - https://una.io
+ * MIT License - https://opensource.org/licenses/MIT
+ *
+ * @defgroup    UnaCore UNA Core
+ * @{
+ */
+
+define('BX_DOL_HEALTHCHECK', 1);
+
+require_once('./inc/header.inc.php');
+require_once(BX_DIRECTORY_PATH_INC . "design.inc.php");
+
+echo 'OK';
+
+/** @} */
