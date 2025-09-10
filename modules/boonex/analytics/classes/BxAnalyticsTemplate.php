99af338515e0f5b404a13dd010426c46dfadc41e
AntonLV <vlnotna@gmail.com>
2025-09-10 13:25:48 +0300

    Ticket #5107 - Upgrade Chart.js to v.4: Analytics.

--- modules/boonex/analytics/classes/BxAnalyticsTemplate.php
+++ modules/boonex/analytics/classes/BxAnalyticsTemplate.php
@@ -24,20 +24,19 @@ class BxAnalyticsTemplate extends BxBaseModGeneralTemplate
         $aTmp = $this->getModule()->getSelectedModules();
         $aModules = $aTmp[0];
         $aModulesList = $aTmp[1];
-        $aModulesList2 = array();
-        foreach($aModules as $sModule){
-            $aModulesList2[] = array('value' => $sModule, 'title' => $aModulesList[$sModule]);
-        }
+        $aModulesList2 = [];
+        foreach($aModules as $sModule)
+            $aModulesList2[] = ['value' => $sModule, 'title' => $aModulesList[$sModule]];
         
         $iDaysBefore = getParam('bx_analytics_default_interval_day');
         $sDate = date('d/m/Y', time() - $iDaysBefore * 86400) . ' - ' . date('d/m/Y');
-        $this->addJs(array('chart.min.js', 'analytics.js', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/daterangepicker/|daterangepicker.js', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/datatables/|datatables.min.js'));
-        $this->addCss(array('main.css', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/daterangepicker/|daterangepicker.css', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/datatables/|datatables.min.css'));
-        return $this->getJsCode('analytics') . $this->parseHtmlByName('canvas.html', array(
-             'bx_repeat:items' => $aModulesList2,
-             'interval' => $sDate,
-             'export_to_csv_title' => _t('_bx_analytics_txt_export_to_csv_title')
-            ));
+        $this->addJs(['chart.umd.min.js', 'chartjs-adapter-moment.min.js', 'analytics.js', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/daterangepicker/|daterangepicker.js', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/datatables/|datatables.min.js']);
+        $this->addCss(['main.css', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/daterangepicker/|daterangepicker.css', BX_DIRECTORY_PATH_MODULES . 'boonex/analytics/plugins/datatables/|datatables.min.css']);
+        return $this->getJsCode('analytics') . $this->parseHtmlByName('canvas.html', [
+            'bx_repeat:items' => $aModulesList2,
+            'interval' => $sDate,
+            'export_to_csv_title' => _t('_bx_analytics_txt_export_to_csv_title')
+        ]);
     }
 }
 
