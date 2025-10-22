732bb4e86a0b89f5c2c3ace1992f3ac87dd4700e
AntonLV <vlnotna@gmail.com>
2025-10-22 16:40:01 +0300

    Modules updates. Back to DEV.

--- modules/boonex/profiler/install/config.php
+++ modules/boonex/profiler/install/config.php
@@ -18,7 +18,7 @@ $aConfig = array(
     'name' => 'bx_profiler',
     'title' => 'Profiler',
     'note' => 'Measure different site timings.',
-    'version' => '15.0.1.DEV',
+    'version' => '15.0.2.DEV',
     'vendor' => 'UNA INC',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
